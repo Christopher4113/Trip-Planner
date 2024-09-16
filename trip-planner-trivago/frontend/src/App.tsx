e647649a925b36e9437e50f463a01968b5a78f9c
Christopher4113 <ch.lam1328@gmail.com>
2024-09-15 23:00:54 -0400

    function that interacts with db

--- trip-planner-trivago/frontend/src/App.tsx
+++ trip-planner-trivago/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import AddTrip from './components/AddTripForm';
 import TripList from './components/TripList';
 import { useCollection,useQuery } from '@squidcloud/react';
 import {Trip} from './types'
+import AskAI from './components/AskAI';
 
 function App() {
   const collection = useCollection<Trip>("trips")
@@ -41,6 +42,7 @@ function App() {
 
   return (
     <div className='card'>
+      <AskAI />
       <AddTrip/>
       <TripList 
       trips={trips.data.map((trip) => trip.data)}
---
+++ trip-planner-trivago/frontend/src/components/AskAI.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useSquid } from "@squidcloud/react";
+import "./AskAI.css"
+
+function AskAI() {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default AskAI
