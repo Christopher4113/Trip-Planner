f025f45e37a7d2b9a3fab4d91717e8ddb6074199
Christopher4113 <ch.lam1328@gmail.com>
2024-09-16 00:08:44 -0400

    we are done

---
+++ trip-planner-trivago/frontend/src/components/LoadingIndicator.tsx
@@ -0,0 +1,10 @@
+
+import "./LoadingIndicator.css"
+function LoadingIndicator() {
+    return (
+        <div className="loading-indicator">
+
+        </div>
+    );
+}
+export default LoadingIndicator
